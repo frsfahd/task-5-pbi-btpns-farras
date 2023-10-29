76e6246897f104c4b21f098a4bf465864eff7967
farraselfahd <farraselfahd2000@gmail.com>
2023-10-29 17:08:49 +0700

    disable some endpoints (users)

--- routes/user.route.go
+++ routes/user.route.go
@@ -10,8 +10,8 @@ import (
 func addUserRoutes(rg *gin.RouterGroup) {
 	user := rg.Group("/users")
 
-	user.GET("/", controllers.UserList)
-	user.GET("/:userId", controllers.UserRetrive)
+	// user.GET("/", controllers.UserList) // testing
+	// user.GET("/:userId", controllers.UserRetrive) // testing
 	user.POST("/register", controllers.UserCreate)
 	user.POST("/login", controllers.UserLogin)
 	user.PUT("/:userId", middleware.Auth, controllers.UserUpdate)    //protected
