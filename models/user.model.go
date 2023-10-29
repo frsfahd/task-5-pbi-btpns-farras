81a63cfb6edf89f449219d327270680e0fe85dc2
farraselfahd <farraselfahd2000@gmail.com>
2023-10-29 07:57:34 +0700

    modify user.model according to new migration

--- models/user.model.go
+++ models/user.model.go
@@ -3,6 +3,6 @@ package models
 type UserSchema struct {
 	// gorm.Model
 	Username string `json:"username"`
-	Email    string `json:"email"`
+	Email    string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
 }
