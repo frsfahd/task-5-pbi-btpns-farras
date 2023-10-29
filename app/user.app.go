fd7b67c577f2609747a59e0f2675b05314b9edd0
farraselfahd <farraselfahd2000@gmail.com>
2023-10-29 17:06:03 +0700

    modify min. password length -> 6

--- app/user.app.go
+++ app/user.app.go
@@ -3,16 +3,16 @@ package app
 type UserRegisterInput struct {
 	Username string `json:"username" validate:"required,alphanum"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
+	Password string `json:"password" validate:"required,min=6"`
 }
 
 type UserUpdateInput struct {
 	Username string `json:"username" validate:"omitempty,alphanum"`
 	Email    string `json:"email" validate:"omitempty,email"`
-	Password string `json:"password" validate:"omitempty,min=8"`
+	Password string `json:"password" validate:"omitempty,min=6"`
 }
 
 type UserLoginInput struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
+	Password string `json:"password" validate:"required,min=6"`
 }
